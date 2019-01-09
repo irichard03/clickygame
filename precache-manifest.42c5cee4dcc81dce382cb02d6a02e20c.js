d0a211c13c0a74b1a170761902a5e88b91ba58ba
Ian Richard <ianrichard03@gmail.com>
2019-01-08 22:51:25 -0600

    Updates

---
+++ precache-manifest.42c5cee4dcc81dce382cb02d6a02e20c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "39531a1c25086940c25d",
+    "url": "/clickygame/static/js/main.39531a1c.chunk.js"
+  },
+  {
+    "revision": "35d4967322379881d30c",
+    "url": "/clickygame/static/js/1.35d49673.chunk.js"
+  },
+  {
+    "revision": "39531a1c25086940c25d",
+    "url": "/clickygame/static/css/main.f3ebbf9b.chunk.css"
+  },
+  {
+    "revision": "14db21a623fcee060fe8621ec6238759",
+    "url": "/clickygame/index.html"
+  }
+];
